384f41bdf22017d69a487c8324e06ef819803013
Mohamed A. H. Faramawy <136634394+MohamedAymanHassan@users.noreply.github.com>
2025-03-30 23:29:10 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,313 @@
+let btn = document.getElementById("already-viewed");
+let img = document.getElementById("current1");
+let img2 = document.getElementById("current2");
+let img3 = document.getElementById("current3");
+let img4 = document.getElementById("current4");
+let img5 = document.getElementById("current5");
+
+function change() {
+    btn.src = "product 1/view 1.jpg";
+    img.style.border = "2px solid black";
+    img.style.padding = "2px";
+    img2.style.border = "none";
+    img2.style.padding = "0";
+    img3.style.border = "none";
+    img3.style.padding = "0";
+    img4.style.border = "none";
+    img4.style.padding = "0";
+    img5.style.border = "none";
+    img5.style.padding = "0";
+}
+function change2() {
+    btn.src = "product 1/view 2.jpg";
+    img.style.border = "none";
+    img.style.padding = "0";
+    img2.style.border = "2px solid black";
+    img2.style.padding = "2px";
+    img3.style.border = "none";
+    img3.style.padding = "0";
+    img4.style.border = "none";
+    img4.style.padding = "0";
+    img5.style.border = "none";
+    img5.style.padding = "0";
+}
+function change3() {
+    btn.src = "product 1/view 3.jpg";
+    img.style.border = "none";
+    img.style.padding = "0";
+    img2.style.border = "none";
+    img2.style.padding = "0";
+    img3.style.border = "2px solid black";
+    img3.style.padding = "2px";
+    img4.style.border = "none";
+    img4.style.padding = "0";
+    img5.style.border = "none";
+    img5.style.padding = "0";
+}
+function change4() {
+    btn.src = "product 1/view 4.png";
+    img.style.border = "none";
+    img.style.padding = "0";
+    img2.style.border = "none";
+    img2.style.padding = "0px";
+    img3.style.border = "none";
+    img3.style.padding = "0";
+    img4.style.border = "2px solid black";
+    img4.style.padding = "2px";
+    img5.style.border = "none";
+    img5.style.padding = "0";
+}
+function change5() {
+    btn.src = "product 1/view 5.jpg";
+    img.style.border = "none";
+    img.style.padding = "0";
+    img2.style.border = "none";
+    img2.style.padding = "0px";
+    img3.style.border = "none";
+    img3.style.padding = "0";
+    img4.style.border = "none";
+    img4.style.padding = "0";
+    img5.style.border = "2px solid black";
+    img5.style.padding = "2px";
+}
+
+const o1 = document.getElementById('o1');
+const o2 = document.getElementById('o2');
+const o3 = document.getElementById('o3');
+const o4 = document.getElementById('o4');
+const o5 = document.getElementById('o5');
+const o6 = document.getElementById('o6');
+const o7 = document.getElementById('o7');
+const o8 = document.getElementById('o8');
+const o9 = document.getElementById('o9');
+
+function choose() {
+  o1.style.border = "2px solid black";
+  o2.style.border = "2px solid rgb(211, 211, 211)";
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose2() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid black";
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose3() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid rgb(211, 211, 211)";
+  o3.style.border = "2px solid black";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose4() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid rgb(211, 211, 211)";
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid black";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose5() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid rgb(211, 211, 211)";
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid black";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose6() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid rgb(211, 211, 211)";
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid black";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose7() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid rgb(211, 211, 211)";
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid black";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose8() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid rgb(211, 211, 211)";
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid black";
+  o9.style.border = "2px solid rgb(211, 211, 211)";
+}
+
+function choose9() {
+  o1.style.border = "2px solid rgb(211, 211, 211)";
+  o2.style.border = "2px solid rgb(211, 211, 211)"
+  o3.style.border = "2px solid rgb(211, 211, 211)";
+  o4.style.border = "2px solid rgb(211, 211, 211)";
+  o5.style.border = "2px solid rgb(211, 211, 211)";
+  o6.style.border = "2px solid rgb(211, 211, 211)";
+  o7.style.border = "2px solid rgb(211, 211, 211)";
+  o8.style.border = "2px solid rgb(211, 211, 211)";
+  o9.style.border = "2px solid black";
+}
+
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
+
+function color1() {
+  btn.src = "product 1/view 1.jpg";
+  img.src = "product 1/view 1.jpg";
+  img.addEventListener('click', function alka() {
+    btn.src = "product 1/view 1.jpg";
+  });
+  img2.src = "product 1/view 2.jpg";
+  img2.addEventListener('click', function alka() {
+    btn.src = "product 1/view 2.jpg";
+  });
+  img3.src = "product 1/view 3.jpg";
+  img3.addEventListener('click', function alka() {
+    btn.src = "product 1/view 3.jpg";
+  });
+  img4.src = "product 1/view 4.png";
+  img4.addEventListener('click', function alka() {
+    btn.src = "product 1/view 4.png";
+  });
+  img5.src = "product 1/view 5.jpg";
+  img5.addEventListener('click', function alka() {
+    btn.src = "product 1/view 5.jpg";
+  });
+
+  c1.style.border = "2px solid black";
+  c2.style.border = "none";
+}
+
+function color2() {
+  btn.src = "product 1/view 1 [2].jpg";
+  img.src = "product 1/view 1 [2].jpg";
+  img.addEventListener('click', function alk() {
+    btn.src = "product 1/view 1 [2].jpg";
+  });
+  img2.src = "product 1/view 2 [2].jpg";
+  img2.addEventListener('click', function alk() {
+    btn.src = "product 1/view 2 [2].jpg";
+  });
+  img3.src = "product 1/view 3 [2].jpg";
+  img3.addEventListener('click', function alk() {
+    btn.src = "product 1/view 3 [2].jpg";
+  });
+  img4.src = "product 1/view 4 [2].jpg";
+  img4.addEventListener('click', function alk() {
+    btn.src = "product 1/view 4 [2].jpg";
+  });
+  img5.src = "product 1/view 5 [2].jpg";
+  img5.addEventListener('click', function alk() {
+    btn.src = "product 1/view 5 [2].jpg";
+  });
+
+  c1.style.border = "none";
+  c2.style.border = "2px solid black";
+}
+
+
+let copyBtn = document.getElementById('copy');
+let anyThing = document.getElementById('right');
+
+copyBtn.addEventListener('click', function() {
+    navigator.clipboard.writeText(right.textContent).then(function() {
+        copyBtn.textContent = "Copied";
+        anyThing.innerHTML = `<i class="fa-solid fa-check"></i>`;
+        right.style.borderRight = "none";
+        setTimeout(function() {
+            copyBtn.innerHTML = '<i class="fa-regular fa-copy"></i>' + " " + 'Copy Code';
+        }, 1500);
+
+        setTimeout(function() {
+            anyThing.textContent = 'EXTRA7';
+            right.style.borderRight = "1px dashed rgb(230, 230, 230)";
+        }, 1500);
+    }).catch(function(error) {
+        console.error('Could not copy text: ', error);
+    });
+});
+
+document.getElementById('x-btn').addEventListener('click', function() {
+    document.getElementById('close-it-when-a-btn-is-clicked').style.display = 'none';
+});
+
+
+
+
+document.querySelector('.add-1').addEventListener('click', function() {
+    const input = document.querySelector('.pro-no-order');
+    input.value = parseInt(input.value) + 1;
+});
+
+document.querySelector('.remove-1').addEventListener('click', function() {
+    const input = document.querySelector('.pro-no-order');
+    const currentValue = parseInt(input.value);
+    if (currentValue > 1) {
+        input.value = currentValue - 1;
+    }
+});
+
+document.querySelectorAll('details').forEach((detail) => {
+  detail.addEventListener('toggle', (event) => {
+    const content = event.target.querySelector('.details-content');
+    if (detail.open) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = '1000';
+    }
+  });
+});
+
+
+let buyNow = document.getElementById('buy-now-btn-to-checkout');
+
+buyNow.onclick = function() {
+  window.location.href = 'checkout.html';
+}
+
+let asd = document.getElementById('logo-img');
+
+asd.onclick = function() {
+  window.location.href = "index.html";
+}
